8dcc0efc1a434ba1481f2d40d4e25daf5b8f866c
Christian Bermeo <czaudiovisual@hotmail.com>
2022-03-26 14:10:28 -0400

    controllers edited for front end

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,7 @@
 class SessionsController < ApplicationController
-
+    
+    skip_before_action :authorize, only: :create
+    
     def create
         # byebug
         user = User.find_by_username(params[:username])
