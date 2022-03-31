e7d0f0ce6c7feecd149a4994cdc916d403428007
Christian Bermeo <czaudiovisual@hotmail.com>
2022-03-31 00:18:54 -0400

    Adding active storage for image uploading

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,10 @@
 class Client < ApplicationRecord
+    include Rails.application.routes.url_helpers
     belongs_to :user
+    has_one_attached :avatar
     has_many :appointments, dependent: :destroy
+
+    def image_url
+    url_for(self.avatar)
+    end
 end
